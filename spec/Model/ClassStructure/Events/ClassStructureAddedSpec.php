cd6fe9dd3218b7c1a5f6010c73a9f337da5dada7
TimiTao <kunicki.tomasz@gmail.com>
2017-01-15 21:05:51 +0100

    add class id

--- spec/Model/ClassStructure/Events/ClassStructureAddedSpec.php
+++ spec/Model/ClassStructure/Events/ClassStructureAddedSpec.php
@@ -3,14 +3,19 @@
 namespace spec\Hexarchium\CodeDomain\Model\ClassStructure\Events;
 
 use Hexarchium\CodeDomain\Event\DomainEventInterface;
+use Hexarchium\CodeDomain\Model\ClassStructure\ClassStructureId;
+use Hexarchium\CodeDomain\Model\ClassStructure\Entity\ClassStructure;
 use Hexarchium\CodeDomain\Model\ClassStructure\Events\ClassStructureAdded;
 use PhpSpec\ObjectBehavior;
 
 class ClassStructureAddedSpec extends ObjectBehavior
 {
-    function let()
+    function let(ClassStructure $classStructure)
     {
-        $this->beConstructedWith(new \DateTime());
+        $classStructure->getId()->willReturn(
+            new ClassStructureId('Class/Id')
+        );
+        $this->beConstructedWith(new \DateTime(), $classStructure);
     }
 
     function it_is_initializable()
@@ -18,4 +23,11 @@ class ClassStructureAddedSpec extends ObjectBehavior
         $this->shouldHaveType(ClassStructureAdded::class);
         $this->shouldBeAnInstanceOf(DomainEventInterface::class);
     }
+
+    function it_should_have_parameter_class_id()
+    {
+        $this->getPayload()->shouldHaveKey('id');
+    }
+
+
 }
--- src/Model/ClassStructure/Events/ClassStructureAdded.php
+++ src/Model/ClassStructure/Events/ClassStructureAdded.php
@@ -3,12 +3,14 @@
 namespace Hexarchium\CodeDomain\Model\ClassStructure\Events;
 
 use Hexarchium\CodeDomain\Event\AbstractDomainEvent;
+use Hexarchium\CodeDomain\Model\ClassStructure\Entity\ClassStructure;
 
 class ClassStructureAdded extends AbstractDomainEvent
 {
 
-    public function __construct(\DateTime $dateTime)
+    public function __construct(\DateTime $dateTime, ClassStructure $classStructure)
     {
         parent::__construct($dateTime);
+        $this->setParam('id', $classStructure->getId()->getId());
     }
 }
